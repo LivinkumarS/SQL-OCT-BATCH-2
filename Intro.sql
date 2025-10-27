30d54de0bd8d4d223171220f0a094eaade4fff76
LivinkumarS <001liv100@ggmail.com>
2025-10-27 14:37:19 +0530

    CRUD --> Create, Read

--- Intro.sql
+++ Intro.sql
@@ -1,1 +1,9 @@
 CREATE DaTaBase student;
+create database dummy;
+
+use student;
+CREATE TABLE agelist (
+id int primary key,
+name varchar(30),
+age int
+);
---
+++ crud.sql
@@ -0,0 +1,7 @@
+
+insert into agelist VALUES
+ (7,'vijay',50);
+ 
+ select * FROM agelist;
+ 
+ update agelist set age=51;
