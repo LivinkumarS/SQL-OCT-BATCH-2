77b9b86c71497150c097d5b4ed64b6d07043fadf
LivinkumarS <001liv100@ggmail.com>
2025-10-28 13:37:55 +0530

    CRUD - Update, Delete; Filtering

---
+++ Filtering.sql
@@ -0,0 +1,8 @@
+
+select * from agelist where age between 49 and 70;
+select * from agelist where age not in (12,34,45,49,56,52,74);
+
+select * from agelist where not name like "%h%";
+
+select * from agelist where id<4 or age>50;
+
--- crud.sql
+++ crud.sql
@@ -2,6 +2,16 @@
 insert into agelist VALUES
  (7,'vijay',50);
  
- select * FROM agelist;
+select * FROM agelist;
  
- update agelist set age=51;
+update agelist set age=51;
+
+use student;
+
+select * from agelist where name="vijay";
+
+UPDATE agelist SET age=52,name="joseph vijay" where id=7;
+
+set sql_safe_updates=1;
+
+DELETE FROM agelist where id=6;
